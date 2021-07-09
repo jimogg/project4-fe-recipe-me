6d518f29e8e5d3f7ce4ae7d347d7805b92da73a8
jimogg <jnogana@gmail.com>
2021-07-09 15:06:24 -0400

    escape punctuations per vercel deploy error msg.

--- components/About.js
+++ components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <section>
                 <h3>About recipeME</h3>
-                <p>RecipeME is a site where any user can sign up and post recipes for the entire online community to enjoy. Signing up give you access to 'my kitchen' where you can track your favorite 'chefs' and create a cookbook of your favorite recipes found on the site! Upload your own recipes so others may delight in your culinary mastery.</p>
+                <p>{`RecipeME is a site where any user can sign up and post recipes for the entire online community to enjoy. Signing up give you access to 'my kitchen' where you can track your favorite 'chefs' and create a cookbook of your favorite recipes found on the site! Upload your own recipes so others may delight in your culinary mastery.`}</p>
             </section>
         </div>
     );
