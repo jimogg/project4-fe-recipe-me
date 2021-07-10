5d1dcd38368c4a03357d74840593a2468ec38fef
jimogg <jnogana@gmail.com>
2021-07-09 21:00:46 -0400

    remove trailing "/" from API_URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://nameless-harbor-22439.herokuapp.com/'
+export const API_URL = 'https://nameless-harbor-22439.herokuapp.com'
 // export const API_URL = 'http://localhost:8000'
